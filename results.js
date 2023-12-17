0370c6eac3308434390fedae779ece09bf81d6c0
Vistulis <145503468+Vistulis@users.noreply.github.com>
2023-12-17 16:57:01 +0000

    Update results.js

--- results.js
+++ results.js
@@ -1,10 +1,14 @@
 import { get } from './IdeologyFinder.js';
 
 const urlParams = new URLSearchParams(window.location.search);
-const rightParam = parseFloat(urlParams.get('right')) || 0; 
-const authParam = parseFloat(urlParams.get('auth')) || 0; 
+const rightParam = parseFloat(urlParams.get('right')) || 0;
+const authParam = parseFloat(urlParams.get('auth')) || 0;
 
-const result = get(rightParam, authParam);
-
-const resultContainer = document.getElementById('result-container');
-resultContainer.textContent = "Ideology match: " + result;
+get(rightParam, authParam)
+  .then(result => {
+    const resultContainer = document.getElementById('result-container');
+    resultContainer.textContent = "Ideology match: " + result;
+  })
+  .catch(error => {
+    console.error('Error fetching ideology data:', error);
+  });
